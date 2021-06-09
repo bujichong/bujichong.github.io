7ca546b94c08f5aee92bba5974d3afe9883f57c8
longxi <longxi@qq.com>
2021-06-09 09:25:30 +0800

    * 更新配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,13 @@ module.exports = {
 	port: 1025,
 //	theme: 'reco',
 //	theme: 'vuepress-theme-maker',
+	locales: {
+		// 键名是该语言所属的子路径
+		// 作为特例，默认语言可以使用 '/' 作为其路径。
+		'/': {
+		lang: 'zh-CN', 
+		},
+	},
 	head: [
 		['meta', {
 			name: 'viewport',
