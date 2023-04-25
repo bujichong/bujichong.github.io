07b055620f00449e1fd56a1304620f4a3cc4357f
bujichong <bujichong@163.com>
2023-04-25 03:28:41 +0000

    Deploying to page from  @ 473b4f2469eba6693f8af01a00f50e33c65b14f4 🚀

---
+++ assets/index.html-18363d62.js
@@ -0,0 +1,17 @@
+import{_ as i,p as l,q as r,s as o,t as n,P as e,G as t,w as d,n as a}from"./framework-d4dfb5a8.js";const u={},_={class:"nav-group-container"},v=o("h2",{class:"nav-group-title"},"pc框架手册",-1),p={class:"nav-container nav",style:{"background-color":"#FBF3E6"}},m={class:"nav-container nav",style:{"background-color":"#FBF0F8"}},h={class:"nav-container nav",style:{"background-color":"#EAEBFD"}},f={class:"nav-container nav",style:{"background-color":"#FBF0F8"}},k={class:"nav-container nav",style:{"background-color":"#FBF0F8"}},x={class:"nav-container nav",style:{"background-color":"#FCEBEB"}},b={class:"nav-container nav",style:{"background-color":"#ECFBEC"}},g={class:"nav-group-container"},F=o("h2",{class:"nav-group-title"},"uni小程序开发手册",-1),B={class:"nav-container nav",style:{"background-color":"#EAEBFD"}},y={class:"nav-container nav",style:{"background-color":"#E9EAEF"}},E={class:"nav-container nav",style:{"background-color":"#e7f2ff"}},S={class:"nav-container nav",style:{"background-color":"#FBF0F8"}};function w(z,C){const c=a("xicons"),s=a("RouterLink");return l(),r("div",null,[o("div",_,[v,o("div",p,[n(c,{class:"xicon",iconSize:"28",icon:"Home"}),o("p",null,[n(s,{to:"/docs/pcwork/index/"},{default:e(()=>[t("首页")]),_:1})])]),o("div",m,[n(c,{class:"xicon",iconSize:"28",icon:"Book"}),o("p",null,[n(s,{to:"/docs/pcwork/1.base/1.files.html"},{default:e(()=>[t("框架基础")]),_:1})])]),o("div",h,[n(c,{class:"xicon",iconSize:"28",icon:"Carbon3DPrintMesh"}),o("p",null,[n(s,{to:"/docs/pcwork/2.css/1.grid.html"},{default:e(()=>[t("样式及图标")]),_:1})])]),o("div",f,[n(c,{class:"xicon",iconSize:"28",icon:"DataTable"}),o("p",null,[n(s,{to:"/docs/pcwork/3.datagrid/1.newGrid.html"},{default:e(()=>[t("数据表格")]),_:1})])]),o("div",k,[n(c,{class:"xicon",iconSize:"28",icon:"DataVis4"}),o("p",null,[n(s,{to:"/docs/pcwork/4.form/1.rules.html"},{default:e(()=>[t("表单")]),_:1})])]),o("div",x,[n(c,{class:"xicon",iconSize:"28",icon:"OpenPanelBottom"}),o("p",null,[n(s,{to:"/docs/pcwork/5.pop/common.html"},{default:e(()=>[t("弹窗")]),_:1})])]),o("div",b,[n(c,{class:"xicon",iconSize:"28",icon:"Basketball"}),o("p",null,[n(s,{to:"/docs/pcwork/6.other/1.uploader.html"},{default:e(()=>[t("其他")]),_:1})])])]),o("div",g,[F,o("div",B,[n(c,{class:"xicon",iconSize:"28",icon:"Home"}),o("p",null,[n(s,{to:"/docs/souni/index/"},{default:e(()=>[t("首页")]),_:1})])]),o("div",y,[n(c,{class:"xicon",iconSize:"28",icon:"DeliveryParcel"}),o("p",null,[n(s,{to:"/docs/souni/1.base/0.first.html"},{default:e(()=>[t("快速上手")]),_:1})])]),o("div",E,[n(c,{class:"xicon",iconSize:"28",icon:"Development"}),o("p",null,[n(s,{to:"/docs/souni/2.components/0.first.html"},{default:e(()=>[t("组件")]),_:1})])]),o("div",S,[n(c,{class:"xicon",iconSize:"28",icon:"Carbon"}),o("p",null,[n(s,{to:"/docs/souni/3.js/0.first.html"},{default:e(()=>[t("Js")]),_:1})])])]),d(` 
+:::: nav-group 综合文档
+::: nav Home
+[首页](./market/index/)
+:::
+::: nav CicsSystemGroup
+[系统相关](./market/1.system/1.vuepower.md)
+:::
+::::
+
+:::: nav-group 蓝芯浏览器
+::: nav Home
+[首页](./bluecore//index/)
+:::
+::: nav Information
+[浏览器安装](./bluecore/1.use/1.setup.md)
+::: `)])}const V=i(u,[["render",w],["__file","index.html.vue"]]);export{V as default};
